e3ad3632d4aa610f008c274f2666e0e5c39e0459
Josemaria <josemarianriagu25@gmail.com>
2019-05-06 18:33:09 +0100

    - Completed arrays.js challenges

--- assignments/objects.js
+++ assignments/objects.js
@@ -86,7 +86,7 @@ console.log(intern2.speak('Hello'));
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-console.log(intern5.multiplyNums(3,4))
+console.log(intern5.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
